c081f14fc10ffd86a2a9d560394bb9e6dad9e465
Gabriel Julio ALves de Souza <gj86225@gmail.com>
2024-02-10 10:48:25 -0300

    ajustando o JS

--- gruntfile.js
+++ gruntfile.js
@@ -58,7 +58,7 @@ module.exports = function(grunt) {
                         },
                         {
                             match: 'ENDERECO_DO_JS',
-                            replace: '../src/scripts/main.js'
+                            replace: './scripts/main..min.js'
                         }
                     ]
                 },
